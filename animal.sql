d286cb5c7a3095f77ac6c8c85b54c1639a496939
tim <timrhymer@ya.ru>
2022-05-01 00:23:24 +0400

    Initial commit

---
+++ animal.sql
@@ -0,0 +1,60 @@
+CREATE table animal_type(
+    id integer primary key autoincrement,
+    name varchar(20)
+);
+
+CREATE table animal_breed(
+    id integer primary key autoincrement,
+    name varchar(30)
+);
+
+CREATE table animal_colors(
+    id integer primary key autoincrement,
+    main_color varchar(30),
+    second_color varchar(30)
+);
+
+CREATE table animal_outcome_subtype(
+    id integer primary key autoincrement,
+    name varchar(20)
+);
+
+CREATE table animal_outcome_type(
+    id integer primary key autoincrement,
+    name varchar(20)
+);
+
+CREATE table animal_outcome_month(
+    id integer primary key autoincrement,
+    name varchar(20)
+);
+
+CREATE table animal_outcome_year(
+    id integer primary key autoincrement,
+    name varchar(20)
+);
+
+CREATE table animals_new(
+    "index" integer primary key autoincrement,
+    age_upon_outcome varchar(20),
+    animal_id varchar(20),
+    name varchar(50),
+    date_of_birth varchar(50),
+    animal_type_id integer,
+    breed_id integer,
+    main_color_id integer,
+    second_color_id integer,
+    outcome_subtype_id integer,
+    outcome_type_id integer,
+    outcome_month_id integer,
+    outcome_year_id integer,
+    FOREIGN KEY (animal_type_id) REFERENCES animal_type(id),
+    FOREIGN KEY (breed_id) REFERENCES animal_breed(id),
+    FOREIGN KEY (main_color_id) REFERENCES animal_colors(id),
+    FOREIGN KEY (second_color_id) REFERENCES animal_colors(id),
+    FOREIGN KEY (outcome_subtype_id) REFERENCES animal_outcome_subtype(id),
+    FOREIGN KEY (outcome_type_id) REFERENCES animal_outcome_type(id),
+    FOREIGN KEY (outcome_month_id) REFERENCES animal_outcome_month(id),
+    FOREIGN KEY (outcome_year_id) REFERENCES animal_outcome_year(id)
+);
+
